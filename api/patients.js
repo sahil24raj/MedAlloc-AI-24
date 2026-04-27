7882871452195fc780aaa846fa65086f6ea6260e
sahil24cu <sr24sahil@gmail.com>
2026-04-27 23:02:28 +0530

    FORCING Vercel functions and testing dummy POST

--- api/patients.js
+++ api/patients.js
@@ -1,2 +1,6 @@
-const app = require('../server/index');
-module.exports = app;
+module.exports = (req, res) => {
+  if (req.method === 'POST') {
+    return res.status(200).json({ status: "POST worked!", body: req.body });
+  }
+  res.status(200).json({ status: "API is alive", method: req.method });
+};
